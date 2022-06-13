7c386d294ed26737a5265a1a3dfd7820d0041a93
Karol Jezierzanski <karoljez97@outlook.com>
2022-06-14 01:04:21 +0200

    Dodaj pliki projektów.

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using WeatherAPI.Models;
+using WeatherAPI.Repositories;
+using System.Net.Http.Json;
+
+namespace WeatherAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private readonly IWeatherRepository _weatherRepository;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public WeatherController(IWeatherRepository weatherRepository, IHttpClientFactory clientFactory)
+        {
+            _weatherRepository = weatherRepository;
+            _clientFactory = clientFactory;
+        }
+
+        [HttpGet("Retrive")]
+        [Authorize(Roles = "Administrator,Guest")]
+        public async Task<ActionResult<WeatherModel>> GetWeather([FromBody]string name)
+        {
+            return await _weatherRepository.Get(name);
+        }
+
+        [HttpPost("Update")]
+        [Authorize(Roles = "Administrator")]
+        public async Task<ActionResult<WeatherModel>> PostWeather([FromBody]string name)
+        {
+            WeatherModel model = new WeatherModel();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"http://api.weatherapi.com/v1/current.json?key= 90da7c4066da4051a93105148221206&q={name}&aqi=no");
+            var client = _clientFactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if(response.IsSuccessStatusCode)
+            {
+                model = await response.Content.ReadFromJsonAsync<WeatherModel>();
+            }
+
+            var newWeather = await _weatherRepository.Create(model);
+            return CreatedAtAction(nameof(GetWeather), new { name = newWeather.location.name }, newWeather);
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "Administrator")]
+        public async Task<ActionResult> PutWeather(string name, [FromBody] WeatherModel model)
+        {
+            if (name != model.location.name)
+            {
+                return BadRequest();
+            }
+
+            await _weatherRepository.Update(model);
+
+            return NoContent();
+        }
+
+        [HttpDelete("Remove")]
+        [Authorize(Roles = "Administrator")]
+        public async Task<ActionResult> Delete([FromBody] string name)
+        {
+            var weatherToDelete = await _weatherRepository.Get(name);
+            if (weatherToDelete == null)
+                return NotFound();
+
+            await _weatherRepository.Delete(weatherToDelete.location.name);
+            return NoContent();
+        }
+
+    }
+}
---
+++ Models/WeatherContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherAPI.Models
+{
+    public class WeatherContext : DbContext
+    {
+        public WeatherContext(DbContextOptions<WeatherContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<WeatherModel> WeatherModel { get; set; }
+    }
+}
