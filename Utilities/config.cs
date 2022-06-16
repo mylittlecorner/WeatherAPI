6f6d35d772859510b184b7d3a3a1f15b7ae4359c
Karol Jezierzanski <karoljez97@outlook.com>
2022-06-16 15:44:04 +0200

    Utilities

---
+++ Utilities/config.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherAPITests.Controllers
+{
+
+    // UWAGA: Wygenerowany kod może wymagać co najmniej programu .NET Framework 4.5 lub .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class config
+    {
+
+        private configApi[] apiField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("api")]
+        public configApi[] api
+        {
+            get
+            {
+                return this.apiField;
+            }
+            set
+            {
+                this.apiField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class configApi
+    {
+
+        private string baseField;
+
+        private string keyField;
+
+        private string nameField;
+
+        /// <remarks/>
+        public string @base
+        {
+            get
+            {
+                return this.baseField;
+            }
+            set
+            {
+                this.baseField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string key
+        {
+            get
+            {
+                return this.keyField;
+            }
+            set
+            {
+                this.keyField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+    }
+
+
+}
